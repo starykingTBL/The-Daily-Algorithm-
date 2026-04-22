2fecec8f2edc80e35a62e1df0e0f0fdcb4dafce2
starykingTBL <abuoluwatobiloba8@gmail.com>
2026-04-22 23:39:34 +0100

    Create src/content/config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title:       z.string(),
+    description: z.string(),
+    pubDate:     z.coerce.date(),
+    author:      z.string().default('The Daily Algorithm'),
+    tags:        z.array(z.string()).default([]),
+    image:       z.string().optional(),
+    draft:       z.boolean().default(false),
+  }),
+});
+
+export const collections = { blog };
