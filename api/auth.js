befe43369714562e6a8f58610390dbf2838206e8
starykingTBL <abuoluwatobiloba8@gmail.com>
2026-04-24 23:40:50 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   const state = Math.random().toString(36).substring(2, 15);
 
   const params = new URLSearchParams({
@@ -11,4 +11,4 @@ export default function handler(req, res) {
   res.redirect(302,
     `https://github.com/login/oauth/authorize?${params.toString()}`
   );
-}
+};
