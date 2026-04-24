a1b0d528a1510082e8fb5d38d7dc5c2dd8c76dcb
starykingTBL <abuoluwatobiloba8@gmail.com>
2026-04-25 00:11:48 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,14 +1,19 @@
-module.exports = function handler(req, res) {
-  const state = Math.random().toString(36).substring(2, 15);
+export default function handler(req, res) {
+  const state    = Math.random().toString(36).substring(2, 15);
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const siteUrl  = process.env.SITE_URL;
+
+  if (!clientId) {
+    res.status(500).send('Missing GITHUB_CLIENT_ID env var');
+    return;
+  }
 
   const params = new URLSearchParams({
-    client_id:    process.env.GITHUB_CLIENT_ID,
-    redirect_uri: `${process.env.SITE_URL}/api/callback`,
+    client_id:    clientId,
+    redirect_uri: siteUrl + '/api/callback',
     scope:        'repo,user',
     state:        state,
   });
 
-  res.redirect(302,
-    `https://github.com/login/oauth/authorize?${params.toString()}`
-  );
-};
+  res.redirect(302, 'https://github.com/login/oauth/authorize?' + params.toString());
+}
