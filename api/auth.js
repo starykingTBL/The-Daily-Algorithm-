3d62e5840c145f45be5dc22ca84c53822eac3dee
starykingTBL <abuoluwatobiloba8@gmail.com>
2026-04-24 23:12:26 +0100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const state = Math.random().toString(36).substring(2, 15);
 
   const params = new URLSearchParams({
-    client_id:    process.env.Ov23lih2FcpzGEUAFncb,
+    client_id:    process.env.GITHUB_CLIENT_ID,
     redirect_uri: `${process.env.SITE_URL}/api/callback`,
     scope:        'repo,user',
     state:        state,
